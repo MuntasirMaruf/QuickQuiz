e6e4442e30c81bde2686bf2dad8e2aa1b6d37361
MuntasirMaruf <muntasir.maruf26@gmail.com>
2025-04-08 19:35:03 +0600

    JavaScript

---
+++ js/myjs.js
@@ -0,0 +1,8 @@
+function showMsg() {
+    document.getElementById("demo1").innerHTML = "Hi There!";
+  }
+
+
+  function showText() {
+    document.getElementById("demo2").innerHTML = document.getElementById("input").value;
+  }
---
+++ views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../js/myjs.js"></script>
+</head>
+<body>
+
+<h2>External JavaScript</h2>
+
+<p id="demo1"></p>
+<button onclick="showMsg()">Show</button>
+<script src="../js/myjs.js"></script>
+
+
+<p id="demo2"></p>
+<input type="text" id="input" onkeyup="showText()">
+
+
+</body>
+</html>
