2a303317d06ed4a429a61c29b1f3a70cbec65450
MuntasirMaruf <muntasir.maruf26@gmail.com>
2025-05-23 22:45:55 +0600

    Exam Page

--- views/regular_exams.php
+++ views/regular_exams.php
@@ -8,6 +8,7 @@ include '../controls/details_student.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Student Dashboard</title>
         <link rel="stylesheet" href="css/studentstyle.css">
+        <link rel="stylesheet" href="css/examstyle.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     </head>
     <body>
@@ -18,7 +19,7 @@ include '../controls/details_student.php';
             <a href="student_history.php" class="exam_link" name='history_btn'><i class="fas fa-history"></i>History</a>
         </div>
         <div class='topnav'>
-            <i class="fas fa-square-caret-left" id="back_btn" name="back_btn"></i>
+            <a href="student_exams.php" id="back_btn" name="back_btn"><i class="fas fa-square-caret-left"></i></a>
             <div class="search_bar">
                 <i class="fas fa-search"></i>
                 <input type="text" id="search_exam" name="search_exam" placeholder="Search for exams...">
@@ -37,20 +38,13 @@ include '../controls/details_student.php';
             </div>
         </div>
         <div class="right_panel">
-            <div class="exam_option_box">
-                <div class="exam_option zoom">
-                    <a href="regular_exams.php" id="exam_option_1" name="exam_option_1"><img src="../res/exam_option.jpg" alt="exam_option_1"></a>
-                    <p id="option_1" name="option_1">Regular</p>
-                </div>
-                <div class="exam_option zoom">
-                    <a href="" id="exam_option_2" name="exam_option_2"><img src="../res/exam_option.jpg" alt="exam_option_2"></a>
-                    <p id="option_2" name="option_2">Regular</p>
-                </div>
-                <div class="exam_option zoom">
-                    <a href="" id="exam_option_3" name="exam_option_3"><img src="../res/exam_option.jpg" alt="exam_option_3"></a>
-                    <p id="option_3" name="option_3">Regular</p>
+            <h1>Regular Exams</h1>
+            <div class="exam_container">
+                <div class="exam_item">
+
                 </div>
             </div>
+            
         </div>
     </body>
 </html>
