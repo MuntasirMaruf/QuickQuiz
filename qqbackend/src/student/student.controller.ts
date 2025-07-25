625f75f3c0041e53d8e4353e9890421542b577ef
MuntasirMaruf <muntasir.maruf26@gmail.com>
2025-07-25 23:44:12 +0600

    Student File Upload

--- qqbackend/src/student/student.controller.ts
+++ qqbackend/src/student/student.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterError, diskStorage } from 'multer';
 import { StudentService } from './student.service';
 import { StudentDto } from './dtos/student.dto';
 
@@ -20,5 +22,38 @@ export class StudentController {
   @UsePipes(new ValidationPipe())
   create(@Body() createStudentDto: StudentDto) : string {
     return this.studentService.create(createStudentDto);
+  }
+
+  // Example of file upload using Multer Uithout validation
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return { message: 'File uploaded successfully', fileName: file.filename };
+  }
+
+  // Example of file upload with validation using Multer
+  @Post("upload/photo")
+  @UseInterceptors(FileInterceptor('photo',
+  {
+    fileFilter: (req, file, callback) => {
+      if (file.originalname.match(/\.(jpg|jpeg|png|jpeg)$/)) {
+        callback(null, true);
+      }
+      else {
+        callback(new MulterError('LIMIT_UNEXPECTED_FILE', 'photo'), false);
+      }
+    },
+    limits: { fileSize: 1024 * 1024 * 5 }, // 5 MB
+    storage: diskStorage({
+      destination: './src/student/uploads',
+      filename: (req, file, callback) => {
+        callback(null, `${Date.now()}-${file.originalname}`);
+      },
+    })
+  }))
+  uploadPhoto(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return { message: 'File uploaded successfully', fileName: file.filename };
   }
 }
