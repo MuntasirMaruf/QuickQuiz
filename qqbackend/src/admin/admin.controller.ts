9ce31ca7dcbfef362ba81da7062dbcbd7892aedf
MuntasirMaruf <muntasir.maruf26@gmail.com>
2025-07-25 23:52:02 +0600

    Student File Upload

---
+++ qqbackend/src/admin/admin.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get,Post, Param,Query,Body } from "@nestjs/common";
+import { AdminService } from "./admin.service";
+import { adminData } from "./admin.dto";
+
+@Controller('admin')
+export class AdminController{
+    constructor(private readonly adminService: AdminService){}
+    @Get()
+    getAdmin(): string{
+        return this.adminService.getAdmin();
+    }
+    @Get('id')
+    getAdminById(@Param('id')admin:number){
+      return this.adminService.getAdminById(admin);
+    }
+     @Get("/find")
+   getAbdullahByNameAndId(@Query('name')name:string,@Query('id')id:number):string{
+     return this.adminService.getAbdullahByNameAndId(name,id);
+   }
+    @Post('/addAdmin')
+    addAdmin(@Body()adminData:object):object{
+        return this.adminService.addAdmin(adminData);
+    }
+    @Post('/addAdmin2')
+    addAdmin2(@Body()addAdminData2:adminData):object{
+        return this.adminService.addAdmin2(addAdminData2);
+    }
+
+    }
--- qqbackend/src/app.module.ts
+++ qqbackend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { AdminModule } from './admin/admin.modlule';
 
 @Module({
-  imports: [StudentModule],
+  imports: [StudentModule,AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
