f2bc46f80f4f66dd621afe64853311a1b9fd37bb
MuntasirMaruf <muntasir.maruf26@gmail.com>
2025-09-01 15:16:00 +0600

    added dynamic route

--- qqfrontend/app/(dashboard)/student/page.tsx
+++ qqfrontend/app/(dashboard)/student/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
-
+            This is Student Page <br /><br />
+            <Link href="/student/Maruf">Maruf</Link> <br /><br />
+            <Link href="/student/Akash">Akash</Link> <br /><br />
         </div>
     )
 }
